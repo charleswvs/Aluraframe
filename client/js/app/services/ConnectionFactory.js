0c9a710d66706fd6ee49bfe1e1b439deb3e1c2bd
Charles Willian <wcharlesw15@gmail.com>
2019-08-26 08:07:06 -0300

    Edits to ConnectionFactory

--- client/js/app/services/ConnectionFactory.js
+++ client/js/app/services/ConnectionFactory.js
@@ -1,40 +1,46 @@
-var stores = ['negociacoes'];
-var version = 4;
-var dbName = 'aluraframe';
+//Module Pattern
+var ConnectionFactory = (function (){
+    var stores = ['negociacoes'];
+    var version = 4;
+    var dbName = 'aluraframe';
 
-class ConnectionFactory{
-    constructor(){
-        throw new Error('Não é possível criar uma instância de ConnectionFactory, é uma classe estática')
+    var connection = null;
+
+    return class ConnectionFactory{
+        constructor(){
+            throw new Error('Não é possível criar uma instância de ConnectionFactory, é uma classe estática')
+        }
+
+        static getConnection(){
+            return new Promise((resolve, reject)=>{
+                let openRequest = window.indexedDB.open(dbName, version);
+
+                openRequest.onupgradeneeded = e =>{
+                    console.log('Cria ou altera um banco');
+                    ConnectionFactory._createStores(e.target.result);
+                }
+
+                openRequest.onsuccess = e =>{
+                    console.log('Conexão estabelicida com sucesso');
+                    if(!connection) connection = e.target.result;
+                    resolve(connection);
+                };
+
+                openRequest.onerror = e => {    
+
+                    console.log(e.target.error);
+
+                    reject(e.target.error.name);
+                };
+            })
+        }
+
+        static _createStores(connection){
+            stores.forEach(store => {
+
+                if(connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
+                connection.createObjectStore(store, { autoIncrement: true });
+            });
+        }
     }
-
-    static getConnection(){
-        return new Promise((resolve, reject)=>{
-            let openRequest = window.indexedDB.open('negociacoes', 4);
-
-            openRequest.onupgradeneeded = e =>{
-                console.log('Cria ou altera um banco');
-                ConnectionFactory._createStores(e.target.result)
-                
-            }
-
-            openRequest.onsuccess = e =>{
-                resolve(e.target.result);
-            };
-
-            openRequest.onerror = e => {    
-
-                console.log(e.target.error);
-
-                reject(e.target.error.name);
-            };
-        })
-    }
-
-    static _createStores(connection){
-        stores.forEach(store => {
-
-            if(connection.objectStoreNames.contains(store)) connection.deleteObjectStore(store);
-            connection.createObjectStore(store, { autoIncrement: true });
-        });
-    }
-}
+}());
