41d65ce722a319ff7613a666894687b98ed83155
Charles Willian <wcharlesw15@gmail.com>
2019-09-02 20:26:10 -0300

    used fetch api at post, in HttpService.js

--- client/js/app/services/HttpService.js
+++ client/js/app/services/HttpService.js
@@ -13,27 +13,11 @@ class HttpService {
     }
 
     post(url, dado) {
-        return new Promise((resolve, reject) => {
-
-            let xhr = new XMLHttpRequest();
-            xhr.open("POST", url, true);// Descobrir o que significa o terceiro parâmetro
-
-            //Especifica o conteúdo que será enviado no POST
-            xhr.setRequestHeader("Content-type", "application/json");
-            xhr.onreadystatechange = () => {
-
-                if (xhr.readyState == 4) {
-
-                    if (xhr.status == 200) {
-
-                        resolve(JSON.parse(xhr.responseText));
-                    } else {
-
-                        reject(xhr.responseText);
-                    }
-                }
-            };
-            xhr.send(JSON.stringify(dado)); // usando JSON.stringifly para converter objeto em uma string no formato JSON.
-        });
+        return fetch(url, {
+            headers: {'Content-type': 'application/json'},
+            method: 'post',
+            body: JSON.stringify(dado)
+        })
+        .then(res -> this._handleErrors(res));
     }
 }
